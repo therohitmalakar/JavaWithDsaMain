60eb86fe5dbec838157f22d3c5a9cbd967604453
therohitmalakar <rohitmalakar437@gmail.com>
2023-12-05 20:42:48 +0530

    java commiting

--- practis/src/CountingSort.java
+++ practis/src/CountingSort.java
@@ -1,2 +1,22 @@
 public class CountingSort {
+    public static void countingSort(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            largest = Math.max(largest,arr[i]);
+        }
+        int[] count = new int[largest+1];
+        for(int i=0; i<arr.length; i++){
+            count[arr[i]]++;
+        }
+        //Sorting
+        int j=0;
+        for(int i=0; i<count.length; i++){
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 1, 3, 2, 4, 3, 7};
+    }
 }
