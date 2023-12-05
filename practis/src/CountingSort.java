3036fe5490e49277945225318eeff10c96f3300a
therohitmalakar <rohitmalakar437@gmail.com>
2023-12-05 20:50:20 +0530

    java commiting

--- practis/src/CountingSort.java
+++ practis/src/CountingSort.java
@@ -11,12 +11,24 @@ public class CountingSort {
         //Sorting
         int j=0;
         for(int i=0; i<count.length; i++){
-
+            while(count[i]>0){
+                arr[j]=i;
+                j++;
+                count[i]--;
+            }
         }
-
+    }
+    public static void printArr(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]);
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         int[] arr = {1, 4, 1, 3, 2, 4, 3, 7};
+        countingSort(arr);
+        printArr(arr);
+
     }
 }
