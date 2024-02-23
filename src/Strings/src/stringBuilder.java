d29ddd5539db4e0b52efeb7f3eb8e218cdb8726e
therohitmalakar <rohitmalakar437@gmail.com>
2024-02-23 17:29:55 +0530

    java commiting

---
+++ src/Strings/src/stringBuilder.java
@@ -0,0 +1,12 @@
+import java.lang.String;
+public class stringBuilder {
+
+    public static void main(String[] args){
+
+        StringBuilder sb = new StringBuilder("");
+        for(char ch='a'; ch<='z'; ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
+    }
+}
